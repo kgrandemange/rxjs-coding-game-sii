824d3b03b731829075b74ca8b4c59452c662bfe1
Kevin Grandemange <kevin@kevingrandemange.net>
2017-06-26 11:21:51 +0200

    fix(ai): format snake case to camel case

--- src/shared/ai.factory.ts
+++ src/shared/ai.factory.ts
@@ -12,11 +12,12 @@ export class AIFactory {
     debug('ai')(`Using '${name}' AI`);
 
     try {
+      // snake case to camel case
+      name = name.replace(/(-\w)/g, (m) => m[1].toUpperCase());
       const AiClass = require(`../ai/${name}`);
       return new AiClass.default(manager);
     } catch(e) {
       throw new Error(`AI '${name}' not found. Add it in src/ai/ folder. Make sure to export default`);
     }
   }
-
 }
